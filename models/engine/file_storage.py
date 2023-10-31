4c12c3c896f58cb94c1bece7ec538a7170d6e3c4
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-31 17:47:05 +1100

    Feat: Added FileStorage class with methods of all, new, save and reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,28 +4,41 @@ import json
 import os
 
 
-class FileStorage():
+class FileStorage:
     """ Serialise or Deserialise data processed"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """ Returns the dictionaries"""
-        return(FileStorage.__objects)
+        return self.__objects
 
     def new(self, obj):
         """create a new object """
-        class_name = type(obj).__name__
-        my.id = obj.id
-        instance_key = class_name + "." + my_id
-        FileStorage.__objects[instance_key] = obj
+        name = type(obj).__name__
+        obj_id = str(obj.id)
+        instance_key = name + "." + obj_id
+        self.__objects[instance_key] = obj
 
     def save(self):
         """ saves in json format to a file """
         my_obj_dict = {}
-        for key in FileStorage.__objects:
-            my_obj_dict[key] = FileStorage.__objects[key].to_dict()
-        with open(FileStorage.__file_path, 'w') as file_path:
-            json.dump(my_obj_dict, file_path)
+        for key in self.__objects:
+            my_obj_dict[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(my_obj_dict, f)
 
-    ## have to do reload
+    def reload(self):
+        """
+        Deserialisation the JSON file to __objects
+        """
+        from models.base_model import BaseModel
+        reloaded_dict = {}
+        try:
+            with open(self.__file_path, "r") as f:
+                reloaded_dict = json.load(f)
+        except:
+            reloaded_dict = {}
+            pass
+        for key, obj in reloaded_dict.items():
+            self.__objects[key] = BaseModel(**obj)
