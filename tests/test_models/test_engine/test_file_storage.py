4ddd901c5d176f688578a7fe495453b3ee73a20b
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-31 21:20:15 +1100

    Feat: Adding unit test for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,8 @@
 Unit tests for FileStorage
 """
 import unittest
+import os
+import json
 from datetime import datetime
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
@@ -17,6 +19,32 @@ class TestFileStorage(unittest.TestCase):
         Setup for the FileStorage class so that it can be initialised
         """
         self.storage = FileStorage()
+        self.temp_file_path = "temp_file.json"
+        self.object_1 = BaseModel(
+            id='123',
+            name='test1',
+            my_number=42,
+            created_at=datetime.now().isoformat(),
+            updated_at=datetime.now().isoformat()
+        )
+        self.object_2 = BaseModel(
+            id='456',
+            name='test2',
+            my_number=24,
+            created_at=datetime.now().isoformat(),
+            updated_at=datetime.now().isoformat()
+        )
+        self.storage.new(self.object_1)
+        self.storage.new(self.object_2)
+        self.storage.save()
+        self.storage.reload()
+
+    def tearDown(self):
+        """
+        Clean up after completion of tests
+        """
+        if os.path.exists(self.temp_file_path):
+            os.remove(self.temp_file_path)
 
     def test_file_path(self):
         """
@@ -51,3 +79,14 @@ class TestFileStorage(unittest.TestCase):
             "Object3": {"id": "3", "name": "Object 3"}
         }
         self.assertEqual(self.storage.all(), expected_output)
+
+    def test_save(self):
+        """
+        Test for save method in FileStorage
+        """
+        self.storage.save()
+        with open(self.storage._FileStorage__file_path, 'r') as f:
+            save_data = json.load(f)
+        objects = self.storage.all()
+        self.assertIn(f"BaseModel.{self.object_1.id}", save_data)
+        self.assertIn(f"BaseModel.{self.object_2.id}", save_data)
