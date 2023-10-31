431ee05c0fe25db6b174382f0f0d4e050d4864b7
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-31 15:09:27 +1100

    added for q3

--- models/base_model.py
+++ models/base_model.py
@@ -7,20 +7,11 @@ from datetime import datetime
 class BaseModel:
     """ BaseModel class"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """ Initialisation of class attributes"""
-        if kwargs:
-            date_format = "%Y-%m-%dT%H:%M:%S.%f"
-            k_dict = kwargs.copy()
-            del k_dict["__class__"]
-            for key in k_dict:
-                if (key == "created_at" or key == "updated_at"):
-                    k_dict[key] = datetime.strptime(k_dict[key], date_format)
-            self.__dict__ = k_dict
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """ Prints given string """
