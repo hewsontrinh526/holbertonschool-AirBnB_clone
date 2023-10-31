28efd0968240d46657d4ad0930038b944ce24658
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-31 17:12:19 +1100

    Fix: Fixing to pass 3 and 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,14 +8,15 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Initialisation of class attributes"""
-        if kwargs:
+        if len(kwargs) != 0:
             date_format = "%Y-%m-%dT%H:%M:%S.%f"
-            k_dict = kwargs.copy()
-            del k_dict["__class__"]
-            for key in k_dict:
-                if (key == "created_at" or key == "updated_at"):
-                    k_dict[key] = datetime.strptime(k_dict[key], date_format)
-            self.__dict__ = k_dict
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, date_format)
+                    setattr(self, key, value)
+                else:
+                    if key != "__class__":
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
