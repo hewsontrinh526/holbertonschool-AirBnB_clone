12791d4060393da74e1d624eb7f082c23d9921ec
EsmeraldaMar <6580@holbertonstudents.com>
2023-11-01 20:26:08 +1100

    added amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Amenity module that represents new amenities """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Inherits from BaseModel"""
+    name = ""
