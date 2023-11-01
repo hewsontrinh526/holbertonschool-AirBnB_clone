df05ee42b21172a073a43e5bd66e4ac0fc8c992f
hewsontrinh526 <6734@holbertonstudents.com>
2023-11-01 18:51:58 +1100

    Feat: Added a create() method

--- console.py
+++ console.py
@@ -9,15 +9,15 @@ import shlex
 
 class HBNBCommand(cmd.Cmd):
     """ Holberton Command prompt to access models' data"""
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
 
     # Quit command method
-    def do_quit(self, arg):
+    def do_quit(self, args):
         """ Quit command to exit program"""
         return True
 
     # EOF command method <Ctrl + D to exit>
-    def do_EOF(self, arg):
+    def do_EOF(self, args):
         """ EOF command to exit the program"""
         print()
         return True
@@ -25,6 +25,21 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Do nothing on empty line + ENTER"""
         pass
+
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel, saves it to the JSON file and
+        prints the id
+        """
+        argument_list = args.split()
+        if len(argument_list) < 1:
+            print("** class name missing **")
+        elif argument_list[0] != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            new_model = BaseModel()
+            new_model.save()
+            print(new_model.id)
 
 
 if __name__ == '__main__':
